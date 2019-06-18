421e809669e5eba04aec4ea8822cd8b6d33b8540
adnan Rahin <adnan.rahin13@gmail.com>
2019-06-17 23:40:13 -0400

    matrix

--- MatrixOperations/matrix.py
+++ MatrixOperations/matrix.py
@@ -4,12 +4,15 @@ Created on Jun 16, 2019
 
 @author: adnan
 '''
-matrix1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-matrix2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-answer = matrix1 * matrix2
-print("Matrix 1")
-print(matrix1)
-print("Matrix 1")
-print(matrix2)
-print("Answer :")
-print(answer)
+
+def matrixmultiplication(mat1, mat2):
+    return np.dot(mat1, mat2)
+
+
+print(matrixmultiplication(np.array([[7, 2, 9], [8, 3, 1], [5, 2, 8]]), np.array([[2, 9, 1], [3, 1, 5], [7, 2, 1]])))
+print(matrixmultiplication(np.array([[1, 3], [2, 4], [0, 5]]), np.array([[1, 0], [2, 3]])))
+u = np.array([[3],[-5],[4]])
+v = np.array([[1],[2],[5]])
+print(v)
+print(u)
+print(u.transpose() * v)
